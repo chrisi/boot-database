83d747d5b1e46f764ea31981f755ab4ffc461cd2
Christian Gebauer <mail@christiangebauer.de>
2015-05-12 22:23:14 +0200

    Webcontroller integriert

---
+++ src/main/java/net/gtidev/sandbox/Application.java
@@ -0,0 +1,11 @@
+package net.gtidev.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringConfig.class, args);
+  }
+}
--- src/main/java/net/gtidev/sandbox/dbaccess/DepartmentRepository.java
+++ src/main/java/net/gtidev/sandbox/dbaccess/DepartmentRepository.java
@@ -4,4 +4,9 @@ import net.gtidev.sandbox.model.Department;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DepartmentRepository extends CrudRepository<Department, Long> {
+
+  int var1 = 0;
+  int var2 = 0;
+
+
 }
